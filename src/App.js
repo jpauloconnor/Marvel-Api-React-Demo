d8531b3eebd635e42f89c046a25129175718fe8f
Paul O'Connor <jamespauloconnor@gmail.com>
2019-01-13 10:36:16 -0500

    Heroku: attempting to resolve missing component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
 import ComicBookSearch from './components/ComicBookSearch';
 import ComicBookDetail from './components/ComicBookDetails';
 import { useComicSearch, useComicDetail } from './hooks/comicBookHookFunctions'; //1
